b182f18fa99b1eefbdd7f5103943cad6c3b73a00
Massa0205 <nmasataka0205@gmail.com>
2022-09-01 22:30:52 +0900

    報告種類のモデル追加

---
+++ demo/src/main/java/com/example/web/model/ReportType.java
@@ -0,0 +1,17 @@
+package com.example.web.model;
+
+
+public class ReportType {
+
+    private int id;
+    private String name;
+
+    public int getId() { return this.id; }
+
+    public String getName() { return this.name; }
+
+    public void setId(int id) { this.id = id; }
+
+    public void setName(String name) { this.name = name; }
+    
+}
