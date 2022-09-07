a43b318ef3353f84bb9a44f9eb3cafe56cc38f8f
Massa0205 <nmasataka0205@gmail.com>
2022-09-07 22:09:20 +0900

    no message

--- demo/src/main/java/com/example/web/controller/IndexController.java
+++ demo/src/main/java/com/example/web/controller/IndexController.java
@@ -1,20 +1,30 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.web.service.ReportTypeService;
+import com.example.web.service.UserService;
+
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ReportTypeService reportTypeService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model){
         /**
          * トップ画面へ
          */
+        model.addAttribute("reportType", reportTypeService.getAllType());
         return "index";
     }
 }
