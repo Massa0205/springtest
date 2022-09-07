f18ecb1d4aa6ff28f4ddcaeb34003c5317920a62
Massa0205 <nmasataka0205@gmail.com>
2022-09-07 22:09:25 +0900

    no message

--- demo/src/main/java/com/example/web/service/ReportTypeService.java
+++ demo/src/main/java/com/example/web/service/ReportTypeService.java
@@ -1,10 +1,16 @@
 package com.example.web.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.web.repository.ReportTypeRepository;
 
+import com.example.web.model.ReportType;
+
 public class ReportTypeService {
     @Autowired
     ReportTypeRepository repository;
+
+    public List<ReportType> getAllType(){ return repository.findAll(); }
 }
